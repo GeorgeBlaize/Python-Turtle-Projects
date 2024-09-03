bfbf96cced8a47523d34315bf61117120ba3eb5b
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2024-09-03 21:01:39 +0600

    Create NikeLogo.py

---
+++ Nike Logo/NikeLogo.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(1)
+color('white')
+bgcolor('black')
+hideturtle()
+
+penup()
+goto(-70,10)
+pendown()
+
+left(180)
+up()
+circle(30,10)
+down()
+begin_fill()
+circle(30,190)
+forward(200)
+left(173)
+forward(190)
+circle(-18,170)
+end_fill()
+done()
