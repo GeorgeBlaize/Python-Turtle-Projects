01e47ac47771cd01c7143f8a2bd61ef5df856bed
GeorgeBlaize <92225843+GeorgeBlaize@users.noreply.github.com>
2025-11-30 16:48:29 +0600

    Create FlowerDesign.py

---
+++ Flower Design/FlowerDesign.py
@@ -0,0 +1,21 @@
+from turtle import *
+import colorsys
+
+hideturtle()
+speed('fastest')
+bgcolor('black')
+width(2)
+
+h=0.0
+
+for i in range(1,50):
+    col=colorsys.hsv_to_rgb(h,1,1)
+    color(col)
+
+    for j in range(10):
+        circle(i*2)
+        right(36)
+
+    h+=0.02
+
+done()
